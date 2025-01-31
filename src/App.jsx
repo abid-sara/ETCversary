aaf137daed2970742293550a65fe4b2b60f5f11f
abid-sara <sara.abid@ensia.edu.dz>
2025-01-31 15:50:01 +0100

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <>
+      <div className="bg-yellowCustom font-primary">Hello this is a test</div>
+      <div className="font-secondary">
+        test text Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Nulla non excepturi vero mollitia enim nobis nostrum expedita neque cum.
+        Unde quidem molestiae harum eos natus amet iste accusamus eveniet in!
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        primary: ["Azonix", "sans-serif"],
+        secondary: ["Montserrat", "sans-serif"],
+      },
+    },
+    colors: {
+      blueCustom: " #021832",
+      yellowCustom: "#FFDF00C2",
+      redCustom: "#BB0000",
+    },
+  },
+  plugins: [],
+};
